d827dee61b9cb614ac883b2accbf6470b19ddfa8
Hepple <william@hepwood.com>
2025-08-11 00:03:43 +0100

    chore: bootstrap Heplink Social

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+﻿export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter Tight", "system-ui", "sans-serif"],
+        display: ["Space Grotesk", "Inter Tight", "sans-serif"],
+      },
+      colors: {
+        heplink: { red: "#ff0000", light: "#ff6666" }
+      }
+    },
+  },
+  plugins: [],
+}
